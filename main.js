a3909333937427f0f1abd113e6b728294eab9e9c
pkoky <sscsk2224@gmail.com>
2021-11-19 16:05:49 +0900

    Add [tailwind.config.js]
    tailwindをCDNではなくインストールして使えるようにした。

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ var main = function () {
     console.log("Hello!");
 };
 main();
+
+// tailwind.config 参照ページ
+// https://shuu1104.com/2021/04/2772/
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        colors: {
+            'regal-blue': '#243c5a',
+            'test-green': '#95DFD6',
+        }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
