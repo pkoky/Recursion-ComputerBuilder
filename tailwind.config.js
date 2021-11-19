c2cc6df6c22d1273984340117587f6bcd052df3a
pkoky <sscsk2224@gmail.com>
2021-11-19 16:40:18 +0900

    Add ['build'] to backage.json
    tailwind.config.jsを編集後のビルドコマンドをpakage.jsonに登録した。実行は[npm rum build]で可能

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ module.exports = {
   theme: {
     extend: {
         colors: {
-            'regal-blue': '#243c5a',
-            'test-green': '#95DFD6',
+            'light-green': '#C8EFEA',
+            'semi-light-green': '#68CFC3',
+            'semi-dark-green': '#1DAF9E',
+            'dark-green': '#009F8C',
+
+            'light-blue': '#CAE7F2',
+            'semi-light-blue': '#6CBAD8',
+            'semi-dark-blue': '#1F91BE',
+            'dark-blue': '#007FB1',
+
+            'light-pink': '#F4D2DE',
+            'semi-light-pink': '#DF81A2',
+            'semi-dark-pink': '#CA3C6E',
+            'dark-pink': '#BF1E56',
         }
     },
   },
